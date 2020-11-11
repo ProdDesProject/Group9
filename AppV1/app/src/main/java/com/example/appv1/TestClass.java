4d66cf9b91983ea4e9bd3d327e526bb8f0245b20
Eloïse Burel <eloise.burel2@gmail.com>
2020-11-11 13:18:38 +0200

    Test comment from eloise

--- AppV1/app/src/main/java/com/example/appv1/TestClass.java
+++ AppV1/app/src/main/java/com/example/appv1/TestClass.java
@@ -1,11 +1,7 @@
 public class TestClass{
     //ignore this class
     //I love potatos
-<<<<<<<<< Temporary merge branch 1
-    // i don t, sry
-    // Hey there
-=========
-    // hello world
 
->>>>>>>>> Temporary merge branch 2
+
+    //I need to know if it still works
 }
