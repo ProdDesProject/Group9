d4512d0f6f3f2fec90da41899fa4f19958438744
Oumaima-Ourkia <oumaima.ourkia@gmail.com>
2020-12-07 13:41:57 +0100

    Access Distant modification

--- FoodAppV1/app/src/main/java/com/example/foodappv1/AcessDistant.java
+++ FoodAppV1/app/src/main/java/com/example/foodappv1/AcessDistant.java
@@ -7,61 +7,64 @@ import org.json.JSONArray;
 
 public class AcessDistant implements AsyncResponse {
     //constant
-    private static final String SERVERADDR="http://stulinux159.ipt.oamk.fi/info.php";
-    public AcessDistant(){
+    private static final String SERVERADDR = "http://stulinux159.ipt.oamk.fi/data.php";
+
+    public AcessDistant() {
         super();
     }
 
 
     /**
-     *
      * return from remote server
+     *
      * @param output
      */
-
-
 
     @Override
     public void processFinish(String output) {
 
-        Log.d("Server", "*********"+output);
+        Log.d("Server", "*********" + output);
         //%
         String[] message = output.split("%");
-        //message[0]=  "save", "last" , "error"
+        //message[0]=  "chooseingredient", "addingredient" ,"showmeal","addmeal", "Error  !"
         //message[1]=  The rest of the message
 
         //2 cases
-        if (message.length>1)
-        {
-            if (message[0].equals("save")){
-                Log.d("save",  "*************"+message[1]);
+        if (message.length > 1) {
+            if (message[0].equals("chooseingredient")) {
+                Log.d("chooseingredient", "*************" + message[1]);
             } else {
-                if (message[0].equals("last")) {
-                    Log.d("last", "*************" + message[1]);
+                if (message[0].equals("addingredient")) {
+                    Log.d("addingredient", "*************" + message[1]);
                 } else {
-                    if (message[0].equals("error")) {
-                        Log.d("error  !", "*************" + message[1]);
+                    if (message[0].equals("showmeal")) {
+                        Log.d("showmeal", "*************" + message[1]);
+                    } else {
+                        if (message[0].equals("addmeal")) {
+                            Log.d("addmeal", "*************" + message[1]);
+                        } else {
+                            if (message[0].equals("error")) {
+                                Log.d("error  !", "*************" + message[1]);
+                            }
+                        }
                     }
                 }
             }
         }
     }
-
-    public void send(String operation, JSONArray jsonData){
+    public void send (String operation, JSONArray jsonData){
         AccessHTTP accessData = new AccessHTTP();
         //link of delegation
         accessData.delegate = this;
         //Add parameter
 
-        accessData.addParam( "operation", operation);
-        accessData.addParam( "The data", jsonData.toString());
+        accessData.addParam("operation", operation);
+        accessData.addParam("theingredients", jsonData.toString());
+        accessData.addParam("themeals", jsonData.toString());
         //Call to server
         accessData.execute(SERVERADDR);
-
     }
 }
-
-
 
 
 
